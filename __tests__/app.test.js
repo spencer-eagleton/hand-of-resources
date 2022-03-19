704f5f76e0d3d55797899a9edd718f20838a5a30
SPENCER EAGLETON <EAGLETONSPENCER@GMAIL.COM>
2022-03-18 18:14:54 -0700

    find by ID test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -34,5 +34,21 @@ describe('hand-of-resources routes', () => {
 
   });
 
-  
+  it('get a bathroom by id', async () => {
+    const expected = {
+      id: '1',
+      name: 'Old Town Portland Loo',
+      status: 'Closed',
+      imageURL:
+        'https://www.portland.gov/sites/default/files/styles/2_1_1600w/public/2020-02/main-gallery.jpg?itok=Rgx9qbwC',
+      lat: '45.528323',
+      long: '122.689120',
+    };
+    const res = await request(app).get('/api/v1/bathrooms/1');
+    expect(res.body).toEqual(expected);
+
+
+  });
+
+
 });
