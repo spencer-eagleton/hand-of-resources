1de8f58b7d99da3a5c728723b7bea036387c198f
SPENCER EAGLETON <EAGLETONSPENCER@GMAIL.COM>
2022-03-19 21:03:43 -0700

    find SRA by id  test passed

--- __tests__/safeRestAreas.test.js
+++ __tests__/safeRestAreas.test.js
@@ -34,4 +34,22 @@ describe('hand-of-resources routes', () => {
     expect(res.body).toEqual(expected);
 
   });
+
+
+  it('gets a safe rest area by id', async () => {
+    const expected = {
+      id: '1',
+      name: 'Gee Creek Safety Rest Area',
+      status: 'Unregulated',
+      imageURL:
+              'https://www.portland.gov/sites/default/files/styles/2_1_1600w/public/2020-02/main-gallery.jpg?itok=Rgx9qbwC',
+      lat: '45.528323',
+      long: '122.689120',
+    };
+    const res = await request(app).get('/api/v1/saferestareas/1');
+    expect(res.body).toEqual(expected);
+
+
+  });
+
 });
