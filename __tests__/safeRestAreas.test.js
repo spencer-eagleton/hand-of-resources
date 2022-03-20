ac8af9d64cb02d8d3a82078a7fa16eab022eba68
SPENCER EAGLETON <EAGLETONSPENCER@GMAIL.COM>
2022-03-19 21:11:52 -0700

    update  SRA by id  test passed

--- __tests__/safeRestAreas.test.js
+++ __tests__/safeRestAreas.test.js
@@ -48,8 +48,26 @@ describe('hand-of-resources routes', () => {
     };
     const res = await request(app).get('/api/v1/saferestareas/1');
     expect(res.body).toEqual(expected);
+  });
+
+  it('updates a safe rest area fetched by Id', async () => {
+   
 
 
+    const res = await request(app).patch('/api/v1/saferestareas/1').send({ status: 'Regulated', });
+
+    const expected = {
+      id: '1',
+      name: 'Gee Creek Safety Rest Area',
+      status: 'Regulated',
+      imageURL:
+                'https://www.portland.gov/sites/default/files/styles/2_1_1600w/public/2020-02/main-gallery.jpg?itok=Rgx9qbwC',
+      lat: '45.528323',
+      long: '122.689120',
+    };
+
+    expect(res.body).toEqual(expected);
+    
   });
 
 });
