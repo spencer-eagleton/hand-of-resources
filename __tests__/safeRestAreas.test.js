e4da580873faa6ff74d53f21c51466512ba57784
SPENCER EAGLETON <EAGLETONSPENCER@GMAIL.COM>
2022-03-19 20:13:19 -0700

    wrote first create test for safe rest areas

---
+++ __tests__/safeRestAreas.test.js
@@ -0,0 +1,29 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('hand-of-resources routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+});
+
+it('creates a safety rest area', async () => {
+  const expected = {
+    name: 'Gee Creek Safety Rest Area',
+    status: 'Unregulated',
+    imageURL:
+              'https://www.portland.gov/sites/default/files/styles/2_1_1600w/public/2020-02/main-gallery.jpg?itok=Rgx9qbwC',
+    lat: '45.528323',
+    long: '122.689120',
+  };
+
+  const response = await request(app).post('/api/v1/saferestareas');
+  expect(response.body).toEqual({ id: expect.any(String), ...expected });
+
+});
