dedf9e098c098c05e3ce834ffbad45bba2323878
SPENCER EAGLETON <EAGLETONSPENCER@GMAIL.COM>
2022-03-19 20:48:49 -0700

    create SRA passes

--- __tests__/safeRestAreas.test.js
+++ __tests__/safeRestAreas.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const SafeRestArea = require('../lib/models/SafeRestArea');
 
 describe('hand-of-resources routes', () => {
   beforeEach(() => {
@@ -11,19 +12,18 @@ describe('hand-of-resources routes', () => {
   afterAll(() => {
     pool.end();
   });
-});
 
-it('creates a safety rest area', async () => {
-  const expected = {
-    name: 'Gee Creek Safety Rest Area',
-    status: 'Unregulated',
-    imageURL:
-              'https://www.portland.gov/sites/default/files/styles/2_1_1600w/public/2020-02/main-gallery.jpg?itok=Rgx9qbwC',
-    lat: '45.528323',
-    long: '122.689120',
-  };
+  it('creates a safety rest area', async () => {
+    const expected = {
+      name: 'Gee Creek Safety Rest Area',
+      status: 'Unregulated',
+      imageURL:
+        'https://www.portland.gov/sites/default/files/styles/2_1_1600w/public/2020-02/main-gallery.jpg?itok=Rgx9qbwC',
+      lat: '45.528323',
+      long: '122.689120',
+    };
 
-  const response = await request(app).post('/api/v1/saferestareas');
-  expect(response.body).toEqual({ id: expect.any(String), ...expected });
-
+    const response = await request(app).post('/api/v1/saferestareas').send(expected);
+    expect(response.body).toEqual({ id: expect.any(String), ...expected });
+  });
 });
