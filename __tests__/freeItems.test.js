f6582475f4ed4e4d645c865564dc0dfdea3cdbee
SPENCER EAGLETON <EAGLETONSPENCER@GMAIL.COM>
2022-03-20 00:29:13 -0700

    update free item by id test passing

--- __tests__/freeItems.test.js
+++ __tests__/freeItems.test.js
@@ -50,4 +50,22 @@ describe('hand-of-resources routes', () => {
     const res = await request(app).get('/api/v1/freeitems/1');
     expect(res.body).toEqual(expected);
   });
+
+  it('updates a free item fetched by Id', async () => {
+    const res = await request(app).patch('/api/v1/freeitems/1').send({ status: 'Unavailable', });
+    const expected = {
+      id: '1',
+      name: 'Wooden Table',
+      status: 'Unavailable',
+      imageURL:
+                'https://www.portland.gov/sites/default/files/styles/2_1_1600w/public/2020-02/main-gallery.jpg?itok=Rgx9qbwC',
+      lat: '45.528323',
+      long: '122.689120',
+    };
+
+    expect(res.body).toEqual(expected);
+    
+  });
+
+
 });
