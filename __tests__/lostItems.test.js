7c413ab645ecb738b7277a1ec2b979a745cf665c
SPENCER EAGLETON <EAGLETONSPENCER@GMAIL.COM>
2022-03-20 00:57:23 -0700

    update lost item by ID test passing

--- __tests__/lostItems.test.js
+++ __tests__/lostItems.test.js
@@ -50,4 +50,20 @@ describe('hand-of-resources routes', () => {
     expect(res.body).toEqual(expected);
   });
 
+  it('updates a lost item fetched by Id', async () => {
+    const res = await request(app).patch('/api/v1/lostitems/1').send({ status: 'Mobile', });
+    const expected = {
+      id: '1',
+      name: '1998 Honda Civic',
+      status: 'Mobile',
+      imageURL:
+                'https://www.portland.gov/sites/default/files/styles/2_1_1600w/public/2020-02/main-gallery.jpg?itok=Rgx9qbwC',
+      lat: '45.528323',
+      long: '122.689120',
+    };
+
+    expect(res.body).toEqual(expected);
+    
+  });
+
 });
